22b34b64092dcce69e271ed2745fd37d589c7e28
ksmit323 <ksmit323@gmail.com>
2025-03-03 15:11:40 +0700

    update config for images to load on the server

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
+// const nextConfig = {
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+//   // If you need to deploy to a custom domain with a CDN, you can use the following:
+//   // assetPrefix: 'https://cdn.greenseedfinancial.com',
+//   // trailingSlash: true,
+// }
+
+// module.exports = nextConfig
+
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true,
+    unoptimized: false,
+    formats: ['image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  // If you need to deploy to a custom domain with a CDN, you can use the following:
-  // assetPrefix: 'https://cdn.greenseedfinancial.com',
-  // trailingSlash: true,
 }
 
 module.exports = nextConfig
-
